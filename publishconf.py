f45ac3b636dbdb32726947f41565837cc2e65dd2
issx77 <issa7791100@gmail.com>
2025-09-15 09:12:07 +0200

    Publication du 15-09-2025 de 9h12

--- publishconf.py
+++ publishconf.py
@@ -9,6 +9,6 @@ from pelicanconf import *
 
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://issx77.github.io/portfolio"
+SITEURL = "https://issx77.github.io/Portfolio-sio"
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
